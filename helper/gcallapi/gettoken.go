c163d4d87b3a42fe4ed0e11dc9f9b7a2bc99df40
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-09 19:50:01 +0700

    balikin lagi

--- helper/gcallapi/gettoken.go
+++ helper/gcallapi/gettoken.go
@@ -15,7 +15,7 @@ func credentialsFromDB(db *mongo.Database) (*oauth2.Config, error) {
 	const credcoll = "credentials"
 	collection := db.Collection(credcoll)
 	var credentialRecord CredentialRecord
-	err := collection.FindOne(context.TODO(), nil).Decode(&credentialRecord)
+	err := collection.FindOne(context.TODO(), bson.M{}).Decode(&credentialRecord)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			return nil, errors.New("tidak ditemukan data cred di collection " + credcoll)
